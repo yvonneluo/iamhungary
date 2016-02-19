daa09f58a0071f7287e1e767b2a9e2ccbf8bdaf5
Yvonne Luo <yvonne@yelp.com>
2016-02-19 15:09:33 -0800

    Working version

--- WhatToEat/CollectionViewController.m
+++ WhatToEat/CollectionViewController.m
@@ -8,6 +8,7 @@
 
 #import "CollectionViewController.h"
 #import "CategoryViewCell.h"
+#import "SwipeViewController.h"
 
 static NSString *kSMInboxMessageDetailsViewCellReuseIdentifier = @"CategoryCellReuseIdentifier";
 static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
@@ -16,6 +17,8 @@ static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
 @property (nonatomic, strong) UILabel * headerLabel;
 @property (nonatomic, strong) UICollectionView *categoriesView;
 @property (nonatomic, strong) NSMutableArray *categoriesName;
+@property (nonatomic, strong) UIActivityIndicatorView *mySpinner;
+
 @end
 
 @implementation CollectionViewController
@@ -24,9 +27,9 @@ static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
     if (self = [super init]) {
         _categoriesName = [[NSMutableArray alloc] init];
         [_categoriesName addObject:@"brunch"];
-        [_categoriesName addObject:@"chinese"];
+        [_categoriesName addObject:@"french"];
         [_categoriesName addObject:@"indian"];
-        [_categoriesName addObject:@"pizza"];
+        [_categoriesName addObject:@"italian"];
         [_categoriesName addObject:@"sushi"];
         [_categoriesName addObject:@"vegetarian"];
     }
@@ -45,8 +48,33 @@ static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     self.collectionView.frame = CGRectMake(0, self.headerLabel.frame.size.height+25, screenBounds.size.width, screenBounds.size.height);
     self.collectionView.backgroundColor = [UIColor whiteColor];
+    _mySpinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    _mySpinner.center = CGPointMake(screenRect.size.width/2, screenRect.size.height/2);
+    _mySpinner.hidesWhenStopped = YES;
+    CGAffineTransform transform = CGAffineTransformMakeScale(4.0f, 4.0f);
+    _mySpinner.transform = transform;
+    _mySpinner.layer.cornerRadius = 05;
+    _mySpinner.opaque = YES;
+    _mySpinner.backgroundColor = [UIColor clearColor];
+    _mySpinner.center = self.view.center;
+    _mySpinner.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    [_mySpinner setColor:[UIColor colorWithRed:231.0/255 green:51.0/255 blue:25.0/255 alpha:1]];//[UIColor colorWithRed:0.6 green:0.8 blue:1.0 alpha:1.0]];
+    [self.view addSubview:_mySpinner];
 }
 
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:YES];
+    _mySpinner.hidden = YES;
+    [_mySpinner stopAnimating];
+
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -84,6 +112,8 @@ static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
     return _headerLabel;
 }
 
+
+
 #pragma mark - UICollectionView DataSource
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
@@ -112,8 +142,42 @@ static UIEdgeInsets SMInboxMessageViewInsets = (UIEdgeInsets){10, 10, 10, 10};
         frame.origin.x = frame.origin.x -35;
         cell.frame = frame;
     }
+
+
     return cell;
 }
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"cell is selected");
+    _mySpinner.hidden = NO;
+    [self.view bringSubviewToFront:_mySpinner];
+    [_mySpinner startAnimating];
+
+    SwipeViewController *swipeViewController = (SwipeViewController *)[self.tabBarController.viewControllers objectAtIndex:0];
+    swipeViewController.searchTerm = _categoriesName[indexPath.row];
+    if ([swipeViewController.searchTerm isEqualToString:@"italian"]) {
+        swipeViewController.searchTerm = @"lolo";
+    }
+    [self performSelector:@selector(selectSearchTerm) withObject:nil afterDelay:0.1];
+
+}
+
+- (void)selectSearchTerm {
+    self.tabBarController.selectedIndex = 0;
+}
+
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
     return UIEdgeInsetsMake(10, 0, 10, 0);
